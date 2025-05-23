a4eacf4c93a0188e0c4bffadefae2f2b57e0728b
nyantama0616 <g2124029@fun.ac.jp>
2025-05-23 18:37:25 +0900

    同じメッセージに返信しないようにした

--- main.py
+++ main.py
@@ -9,38 +9,44 @@ app = App(
 
 gemini_senpai = GeminiSenpai()
 
+replied_messages = set()
+
 @app.event("reaction_added")
 def handle_reaction(event, client, logger):
   reaction = event["reaction"]
   channel = event["item"]["channel"]
   timestamp = event["item"]["ts"]
 
-  logger.info(f"Reaction '{reaction}' added to message {timestamp} in channel {channel}")
+  if reaction != "gemini-homehome":
+    return
 
   result = client.conversations_history(
-      channel=channel,
-      latest=timestamp,
-      inclusive=True,
-      limit=1
+    channel=channel,
+    latest=timestamp,
+    inclusive=True,
+    limit=1
   )
 
   messages = result.get("messages", [])
   if not messages:
-      logger.warn("Message not found.")
-      return
+    logger.warn("Message not found.")
+    return
+  
+  message_timestamp = messages[0]["ts"]
+  if message_timestamp in replied_messages:
+    return
+  replied_messages.add(message_timestamp)
   
   original_message = messages[0]["text"]
   logger.info(f"Original message: {original_message}")
 
   gemini_response = gemini_senpai.send(original_message)
 
-  if reaction == "gemini-homehome":
-    client.chat_postMessage(
-        channel=channel,
-        thread_ts=timestamp,  # スレッドとして返信
-        text=gemini_response,
-    )
-
+  client.chat_postMessage(
+    channel=channel,
+    thread_ts=timestamp,
+    text=gemini_response,
+  )
+  
 if __name__ == "__main__":
-  # アプリを起動して、ソケットモードで Slack に接続します
   SocketModeHandler(app, SLACK_APP_TOKEN).start()
